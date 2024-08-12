1d7281f55d4e609bf08cf7416157b8091e685d4b
HariPrashand <harikeshav76@gmail.com>
2024-08-12 18:55:51 +0530

    Update streamlit_app.py

--- streamlitapp/streamlit_app.py
+++ streamlitapp/streamlit_app.py
@@ -17,7 +17,7 @@ def get_base64_of_bin_file(bin_file):
         data = f.read()
     return base64.b64encode(data).decode()
 
-img_file = './pages/image1.jpg'
+img_file = 'image1.jpg'
 img_base64 = get_base64_of_bin_file(img_file)
 
 page_bg_img = f'''
